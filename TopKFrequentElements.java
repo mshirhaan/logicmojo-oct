06d1efd33179cf7a5dc6052ea22915fdf559e5d6
Mohammed Shirhaan <53033299+mshirhaan@users.noreply.github.com>
2023-10-22 11:52:51 +0530

    Update TopKFrequentElements.java

--- TopKFrequentElements.java
+++ TopKFrequentElements.java
@@ -23,3 +23,44 @@ class Solution {
       return result;
     }
 }
+
+
+//using buckets
+
+class Solution {
+   public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            if (!map.containsKey(num)) {
+                map.put(num, 0);
+            }
+            map.put(num, map.get(num) + 1);
+        }
+
+        List<List<Integer>> bucket = new ArrayList<>();
+        for (int i = 0; i <= nums.length; i++) {
+            bucket.add(new ArrayList<>());
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            bucket.get(entry.getValue()).add(entry.getKey());
+        }
+
+        int result[] = new int[k];
+
+        int count = 0;
+
+        for (int i = bucket.size() - 1; i >= 0; i--) {
+            List<Integer> mug = bucket.get(i);
+            for (int j = 0; j < mug.size(); j++) {
+                result[count++] = mug.get(j);
+                if (count == k) {
+                    return result;
+                }
+            }
+        }
+
+        return null;
+    }
+}
