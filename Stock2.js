99aabd5c5eac86530814dfe01afe1743779958e1
Mohammed Shirhaan <53033299+mshirhaan@users.noreply.github.com>
2024-02-17 11:39:21 +0530

    Create Stock2.js

---
+++ Stock2.js
@@ -0,0 +1,10 @@
+var maxProfit = function(prices) {
+    let totalProfit = 0;
+
+    for(let i = 1; i < prices.length; i++) {
+        if(prices[i] > prices[i-1]) {
+            totalProfit+= prices[i] - prices[i-1]
+        }
+    }
+    return totalProfit
+};
